9b0c38dbb2ec520f0a7d55b1edafa16c655bf6e3
dangrant6 <dwgkid6@gmail.com>
2023-05-27 21:02:42 -0400

    r

--- memeapp/memeapp/settings.py
+++ memeapp/memeapp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,16 +80,7 @@ WSGI_APPLICATION = 'memeapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("NAME"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("PORT"),
-    }
+    "default" : dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 
