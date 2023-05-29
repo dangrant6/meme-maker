d43b75c71cc52b97bd818c76d83406270e4e64c7
dangrant6 <dwgkid6@gmail.com>
2023-05-28 21:45:30 -0400

    oops

--- memeapp/settings.py
+++ memeapp/settings.py
@@ -83,10 +83,8 @@ WSGI_APPLICATION = 'memeapp.wsgi.application'
 
 DATABASES = {
     "default":{
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'DATABASE': os.getenv("DATABASE"),
+        'NAME': os.getenv("NAME"),
         'USER': os.getenv("USER"),
         'PASSWORD': os.getenv("PASSWORD"),
         'HOST': os.getenv("HOST"),
